17255a5f99f1f3b2746f4c481cb018f6e723552a
Naor Livne <naorlivne@gmail.com>
2017-08-16 19:28:37 +0300

    fixing breaking bug with devices

--- nebulactl.py
+++ nebulactl.py
@@ -4,7 +4,7 @@ from NebulaPythonSDK import Nebula
 from os.path import expanduser
 
 
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 
 
 # i'm separating the nebulactl.py to 2 parts, the first is the NebulaCall class below which is going to be in charge of
