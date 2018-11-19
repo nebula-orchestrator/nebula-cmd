8fe7a0cbe0d1770cc108013569d067ad02394dc0
jakobpederson <jakob.pederson@gmail.com>
2018-11-19 08:06:09 -0600

    wip

--- upload-nebula.sh
+++ upload-nebula.sh
@@ -1,10 +1,10 @@
 #go to home and setup git
 cd $HOME
-git config --global user.email "user-email@email.com"
-git config --global user.name "user-name"
+git config --global user.email $USER_EMAIL
+git config --global user.name $USER_NAME
 
 #clone the repository in the buildApk folder
-git clone --quiet --branch=master  https://user-name:$GITHUB_API_KEY@github.com/user-name/nebula-cmd  master > /dev/null
+git clone --quiet --branch=master  https://$USER_NAME:$GITHUB_API_KEY@github.com/$USER_NAME/nebula-cmd  master > /dev/null
 
 cd master
 pyinstaller -F nebulactl.py
@@ -12,7 +12,7 @@ pyinstaller -F nebulactl.py
 #add, commit and push files
 git add -f .
 git remote rm origin
-git remote add origin https://user-name:$GITHUB_API_KEY@github.com/user-name/nebula-cmd.git
+git remote add origin https://$USER_NAME:$GITHUB_API_KEY@github.com/$USER_NAME/nebula-cmd.git
 git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed - nebulactl.py run and pushed"
 git push -fq origin master > /dev/null
