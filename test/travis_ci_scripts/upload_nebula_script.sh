45ffa8f3ba47b703b77fc8edb94d8ed611fe8d6e
jakobpederson <jakob.pederson@gmail.com>
2019-02-13 17:58:23 -0600

    Enhancement: add shell build script. skip travis

---
+++ test/travis_ci_scripts/upload_nebula_script.sh
@@ -0,0 +1,25 @@
+#go to home and setup git
+cd $HOME
+git config --global user.email $USER_EMAIL
+git config --global user.name $USER_NAME
+
+#clone the repository in the buildApk folder
+git clone --quiet --branch=$TRAVIS_BRANCH  https://$USER_NAME:$GITHUB_API_KEY@github.com/$USER_NAME/nebula-cmd  master > /dev/null
+
+cd master
+pyinstaller -F nebulactl.py
+
+MESSAGE=$(git log -1 HEAD --pretty=format:%s)
+
+if [[ "$MESSAGE" == *"Travis build"* ]] || [[ "$MESSAGE" == *"skip travis"* ]]; then
+    echo "already pushed"
+else
+    #add, commit and push files
+    git add -f .
+    git remote rm origin
+    git remote add origin https://$USER_NAME:$GITHUB_API_KEY@github.com/$USER_NAME/nebula-cmd.git > /dev/null 2>&1
+    git add -f .
+    git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed - nebulactl.py run and pushed"
+    git push --quiet --set-upstream origin $TRAVIS_BRANCH
+    echo -e "Done"
+fi
