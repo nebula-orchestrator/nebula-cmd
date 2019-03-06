6d9ef5873c70846b324ea0a2e449fd5135ec2f4b
Naor Livne <naor@mintigo.com>
2019-03-06 10:36:15 +0200

    udding missing variables for travis autobuild deployments

--- test/travis_ci_scripts/upload_nebulactl.sh
+++ test/travis_ci_scripts/upload_nebulactl.sh
@@ -5,7 +5,7 @@ git config --global user.email $USER_EMAIL
 git config --global user.name $USER_NAME
 
 #clone the repository in the buildApk folder
-git clone --quiet --branch=$TRAVIS_BRANCH  https://$USER_NAME:$GITHUB_API_KEY@github.com/$USER_NAME/nebula-cmd  master > /dev/null
+git clone --quiet --branch=$TRAVIS_BRANCH  https://$USER_NAME:$GITHUB_API_KEY@github.com/$USER_NAME/nebula-cmd  master > /dev/null 2>&1
 
 cd master
 pyinstaller -F `pwd`/nebulactl.py
@@ -15,7 +15,7 @@ MESSAGE=$(git log -1 HEAD --pretty=format:%s)
 if [[ "$MESSAGE" == *"RUN UPLOAD SCRIPT"* ]]; then
     #add, commit and push files
     git remote rm origin
-    git remote add origin https://$USER_NAME:$GITHUB_API_KEY@github.com/$USER_NAME/nebula-cmd.git > /dev/null 2>&1
+    git remote add origin https://$USER_NAME:$GITHUB_API_KEY@github.com/nebula-orchestrator/nebula-cmd.git > /dev/null 2>&1
     git add dist
     git commit -m 'skip travis build $TRAVIS_BUILD_NUMBER pushed - nebulactl.py run and pushed'
     git push --quiet --set-upstream origin $TRAVIS_BRANCH
