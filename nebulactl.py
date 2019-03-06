02c7f1cdbf7f59f980a67714a47069731031e566
Naor Livne <naor@mintigo.com>
2019-03-06 16:01:47 +0200

    adding support for token auth

--- nebulactl.py
+++ nebulactl.py
@@ -3,7 +3,7 @@ import click, json, ast, os, base64
 from NebulaPythonSDK import Nebula
 from os.path import expanduser
 
-VERSION = "2.2.0"
+VERSION = "2.2.1"
 
 
 # i'm separating the nebulactl.py to 2 parts, the first is the NebulaCall class below which is going to be in charge of
@@ -16,9 +16,19 @@ class NebulaCall:
             home = expanduser("~")
             auth_file = open(home + "/.nebula.json", "r")
             auth_json = json.load(auth_file)
-            self.connection = Nebula(username=auth_json["username"],
-                                     password=base64.b64decode(auth_json["password"].encode('utf-8')).decode('utf-8'),
-                                     host=auth_json["host"], port=auth_json["port"], protocol=auth_json["protocol"])
+            for key, value in auth_json.items():
+                if value == "":
+                    auth_json[key] = None
+            if auth_json["password"] is None:
+                nebula_pass = None
+            else:
+                nebula_pass = base64.b64decode(auth_json["password"].encode('utf-8')).decode('utf-8')
+            if auth_json["token"] is None:
+                nebula_token = None
+            else:
+                nebula_token = base64.b64decode(auth_json["token"].encode('utf-8')).decode('utf-8')
+            self.connection = Nebula(username=auth_json["username"],  host=auth_json["host"], port=auth_json["port"],
+                                     token=nebula_token, password=nebula_pass, protocol=auth_json["protocol"])
         except Exception as e:
             click.echo(click.style(e, fg="red"))
             click.echo(click.style("error reading ~/nebula.json auth file, try logging in first", fg="red"))
@@ -254,19 +264,22 @@ def reports(page_size, hostname, device_group, report_creation_time_filter, repo
 # creates a cred file at ~/.nebula.json with the auth credentials or updates it's values if it exists
 @nebulactl.command(help="login to nebula")
 @click.option('--username', '-u', prompt='what is nebula manager basic auth username?',
-              help='nebula manager basic auth username')
+              help='nebula manager basic auth username', default="")
 @click.option('--password', '-p', prompt='what is nebula manager basic auth password?', hide_input=True,
-              confirmation_prompt=True, help='nebula manager basic auth password')
+              confirmation_prompt=True, help='nebula manager basic auth password', default="")
+@click.option('--token', '-t', prompt='what is nebula manager auth token?', hide_input=True,
+              confirmation_prompt=True, help='nebula manager auth token', default="")
 @click.option('--host', '-h', prompt='what is nebula manager host?', help='nebula manager host.')
 @click.option('--port', '-c', prompt='what is nebula manager port?', help='nebula manager port, defaults to 80',
               default=80, type=click.IntRange(1, 65535))
 @click.option('--protocol', '-P', prompt='what is nebula manager protocol?', default="http",
               help='nebula manager protocol, defaults to http')
-def login(username, password, host, port, protocol):
+def login(username, password, token, host, port, protocol):
     home = expanduser("~")
     auth_file = open(home + "/.nebula.json", "w+")
     json.dump({"username": username, "password": base64.b64encode(password.encode('utf-8')).decode('utf-8'),
-               "host": host, "port": port, "protocol": protocol}, auth_file)
+               "token": base64.b64encode(token.encode('utf-8')).decode('utf-8'), "host": host, "port": port,
+               "protocol": protocol}, auth_file)
     auth_file.write('\n')
 
 
